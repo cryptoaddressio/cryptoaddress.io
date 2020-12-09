8d506c05f8f31954673f81b730905bd7eea63605
Lucas Munhoz <ln.munhoz@gmail.com>
2020-12-09 23:06:00 +0700

    Init

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className={styles.footer}>Made with 🦊 for you</footer>
+    </>
+  );
+}
